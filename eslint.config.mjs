eeed4c56fa8461eeb9b3f1dd46540558684f4944
shahadathhs <shahadathhossensajib732@gmail.com>
2026-01-17 12:23:47 +0600

    chore: setup completed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
 import { baseConfig } from '@vortex/eslint-config/base.js';
+import { expressConfig } from '@vortex/eslint-config/express.js';
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
   {
-    ignores: ['services/**', 'packages/**', 'gateway/**'],
+    ignores: ['**/dist/**', '**/node_modules/**', '**/.turbo/**'],
   },
   ...baseConfig,
+  ...expressConfig.map((config) => ({
+    ...config,
+    files: ['services/**/*.{js,ts,tsx}', 'gateway/**/*.{js,ts,tsx}'],
+  })),
 ];
